2a4ee42e21c7e629ef44ec2f9de244a71c1a77b4
trifke87 <42452032+trifke87@users.noreply.github.com>
2020-04-19 15:16:38 +0200

    first commit

---
+++ GigHub/ViewModels/ExternalLoginConfirmationViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GigHub.ViewModels
+{
+    public class ExternalLoginConfirmationViewModel
+    {
+        [Required]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+    }
+}
