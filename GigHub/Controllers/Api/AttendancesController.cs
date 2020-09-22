a4364a99d58c4f0f79b9a1f7fbf4a15f118f72bd
trifke <mtrifke@yahoo.com>
2020-09-22 23:27:37 +0200

    [GIG-003] labels and cleaning project

--- GigHub/Controllers/Api/AttendancesController.cs
+++ GigHub/Controllers/Api/AttendancesController.cs
@@ -8,7 +8,7 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 
-namespace GigHub.Controllers
+namespace GigHub.Controllers.Api
 {
     [Authorize]
     public class AttendancesController : ApiController
--- GigHub/Controllers/Api/GigsController.cs
+++ GigHub/Controllers/Api/GigsController.cs
@@ -25,6 +25,9 @@ namespace GigHub.Controllers.Api
             var userId = User.Identity.GetUserId();
             var gig = _context.Gigs.Single(g => g.Id == id && g.ArtistId == userId);
 
+            if (gig.IsCanceled)
+                return NotFound();
+
             gig.IsCanceled = true;
             _context.SaveChanges();
 
