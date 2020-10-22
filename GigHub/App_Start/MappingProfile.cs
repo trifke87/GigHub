823aeec56f7622afd252dfde58c5e1f96e2800bc
trifke <mtrifke@yahoo.com>
2020-10-23 01:04:24 +0200

    [GIG-006] autoMapper

---
+++ GigHub/App_Start/MappingProfile.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using GigHub.Dtos;
+using GigHub.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GigHub.App_Start
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            //MapperConfiguration config = new MapperConfiguration(cfg =>
+            //{
+            //    cfg.CreateMap<ApplicationUser, UserDto>();
+            //    cfg.CreateMap<Gig, GigDto>();
+            //    cfg.CreateMap<Notification, NotificationDto>();
+            //});
+
+            CreateMap<ApplicationUser, UserDto>();
+            CreateMap<Gig, GigDto>();
+            CreateMap<Notification, NotificationDto>();
+        }
+
+    }
+}
---
+++ GigHub/Dtos/NotificationDto.cs
@@ -0,0 +1,18 @@
+﻿using GigHub.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GigHub.Dtos
+{
+    public class NotificationDto
+    {
+        public DateTime DateTime { get; private set; }
+        public NotificationType Type { get; private set; }
+        public DateTime? OriginalDateTime { get; private set; }
+        public string OriginalVanue { get; private set; }
+        public GigDto Gig { get; private set; }
+
+    }
+}
