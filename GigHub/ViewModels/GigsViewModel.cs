3e751f8f2572c130d5035abe97c4ff7dd2fd196b
trifke <mtrifke@yahoo.com>
2020-11-11 21:25:35 +0100

    [GIG-010] search vanue

--- GigHub/ViewModels/GigsViewModel.cs
+++ GigHub/ViewModels/GigsViewModel.cs
@@ -11,5 +11,6 @@ namespace GigHub.ViewModels
         public IEnumerable<Gig> UpcomingGigs { get; set; }
         public bool ShowActions { get; set; }
         public string Heading { get; set; }
+        public string SearchTerm { get; set; }
     }
 }
