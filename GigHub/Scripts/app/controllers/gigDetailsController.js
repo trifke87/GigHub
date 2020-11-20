43b5b9cc5d9ce43622ffcc1291c9bf2b070d0ebe
trifke <mtrifke@yahoo.com>
2020-11-20 16:02:14 +0100

    [GIG-012] following button

---
+++ GigHub/Scripts/app/controllers/gigDetailsController.js
@@ -0,0 +1,37 @@
+﻿var GigDetailsController = function (followingService) {
+    var followingButton;
+    var init = function () {
+
+    };
+
+    var toggleFollowing = function () {
+        followingButton = $(e.target);
+
+        var follweeId = followingButton.attr("data-user-id");
+
+        if (followingButton.hasClass("btn-default"))
+
+    };
+
+    var done = function () {
+        var text = (followingButton.text() == "Follow") ? "Following" : "Follow";
+        followingButton.toggleClass("btn-info").toggleClass("btn-default").text(text);
+    };
+
+    var fail = function () {
+        alert("Something failed");
+    };
+
+    return {
+        init: init
+    }
+
+}(FollowingService);
+
+
+$(".js-toggle-follow").click(function (e) {
+    
+   
+  
+    
+})
