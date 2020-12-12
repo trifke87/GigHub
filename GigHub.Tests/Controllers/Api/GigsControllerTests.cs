88c05c3bf91961812ea69ce8236d9070c7f6d9d0
trifke <mtrifke@yahoo.com>
2020-12-12 23:41:54 +0100

    [GIG-014] mock and testing

---
+++ GigHub.Tests/Controllers/Api/GigsControllerTests.cs
@@ -0,0 +1,39 @@
+﻿using GigHub.Controllers;
+using GigHub.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Security.Claims;
+using System.Security.Principal;
+
+namespace GigHub.Tests.Controllers.Api
+{
+    [TestClass]
+    public class GigsControllerTests
+    {
+        private GigsController controller;
+        public GigsControllerTests()
+        {
+            var identity = new GenericIdentity("user1@domain.com");
+            identity.AddClaim(
+                new Claim("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name", "user1@domain.com")
+                );
+            identity.AddClaim(
+                new Claim("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier", "1")
+                );
+
+            var principal = new GenericPrincipal(identity, null);
+
+            var mockUoW = new Mock<IUnitOfWork>();
+
+            controller = new GigsController(mockUoW.Object);
+
+            //controller.User = principal;
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ GigHub.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GigHub.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GigHub.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5d08a06b-a63b-4032-8846-f36bfcc353ad")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
