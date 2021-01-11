5666faff4d62c3ea6e5a6bbde54feefce48e0e26
trifke <mtrifke@yahoo.com>
2021-01-11 23:55:01 +0100

    [GIG-017] setup integration tests

---
+++ GigHub.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using System;
+
+namespace GigHub.IntegrationTests
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+        }
+    }
+}
